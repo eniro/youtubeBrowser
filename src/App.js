a6bc3660e9b9b622db89717117b3ae7aa19025ea
Iro Enezli <ienezli@stepone.gr>
2018-11-08 11:10:34 +0200

    added App state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyBtTTddh_6G0JFu_r2k0LJaxy4-BwC3b3Q';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
   render() {
     return (
       <div>
